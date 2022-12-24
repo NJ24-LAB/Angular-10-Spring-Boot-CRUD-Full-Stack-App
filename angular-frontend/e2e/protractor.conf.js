12d7ff5bedaad1cb2eee2a756ad2e3a3bb490a15
NJ24-LAB <81625019+NJ24-LAB@users.noreply.github.com>
2022-12-24 13:56:53 +0530

    Update protractor.conf.js

--- angular-frontend/e2e/protractor.conf.js
+++ angular-frontend/e2e/protractor.conf.js
@@ -16,7 +16,7 @@ exports.config = {
     browserName: 'chrome'
   },
   directConnect: true,
-  baseUrl: 'http://localhost:4200/',
+  baseUrl: 'http://3.110.45.192:4200/',
   framework: 'jasmine',
   jasmineNodeOpts: {
     showColors: true,
